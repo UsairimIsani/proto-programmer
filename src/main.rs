94d1b4d12a8a98d6325595376f50c039aaf7581f
Usairim Isani <usairim.isani@gmail.com>
2019-09-13 00:05:46 +0500

    Removed some illogical code.

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,6 @@ use std::io::prelude::*;
 use std::io::BufReader;
 fn main() {
     let _config: Config = configure();
-    let byte = 0x89;
-    let bytes_array: Vec<u8> = convert_bits_to_bytes(byte);
-    println!("{:?}", &bytes_array);
-    for i in bytes_array.iter() {
-        print!("{:b}", i);
-    }
-    println!();
     let file = File::open("/home/usairim/proto-programmer/binary-xilinx.bin").unwrap();
     let buf_reader = BufReader::new(file);
     let bytes = buf_reader.bytes();
@@ -28,6 +21,6 @@ fn main() {
         .cloned()
         .collect();
     let mut bang_file = File::create("/home/usairim/proto-programmer/bang.bang").unwrap();
-    bang_file.write_all(&mut &new_byte_array);
-    print!("{:?} {:?}", new_byte_array, bang_file);
+    bang_file.write_all(&mut &new_byte_array).unwrap();
+    // print!("{:?} {:?} {:?}", new_byte_array, bang_file, success);
 }
